3e8fc3438da0b4a2dc1abc3d10e60aa89ed9b238
Gabriel De Luca <caprieldeluca@gmail.com>
2024-09-22 08:15:51 -0300

    Start development of version 0.3.

--- tactilemaps/__init__.py
+++ tactilemaps/__init__.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
+"""Import the plugin."""
 __copyright__ = '(C) 2023 by Laboratorio de Geociencias - FIE'
 __email__ = 'geociencias@fie.undef.edu.ar'
 __license__ = 'GPL version 3'
 
-def classFactory(iface):
+def classFactory(iface):  # pylint: disable=invalid-name
     """Factory method for the plugin object."""
-    from tactilemaps.tactilemaps_plugin import TactileMapsPlugin
+    from tactilemaps.tactilemaps_plugin import TactileMapsPlugin  # pylint: disable=import-outside-toplevel
     return TactileMapsPlugin(iface)
-
